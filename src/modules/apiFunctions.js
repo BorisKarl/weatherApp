717cbc8f6a98179af66b133dcc5ac7a2aac9d34d
BorisKarl <herrnielsen@web.de>
2024-04-08 15:51:07 +0200

    added api key

--- src/modules/apiFunctions.js
+++ src/modules/apiFunctions.js
@@ -1,6 +1,5 @@
-const api_key = "xxx";
 const url = "https://api.openweathermap.org/data/2.5/weather?q=";
-const urlContinue = "xxx";
+const urlContinue = "&APPID=2398f3d056c8d161f94aadd37333d0d3&units=metric";
 
 const content = document.getElementById("valueContent");
 const cityDOM = document.getElementById("city");
